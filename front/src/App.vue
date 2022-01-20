4de4dc4bad0757297a47274cb608a0d0f77fd31c
danANDla <d.a.n.202021@gmail.com>
2022-01-20 12:05:38 +0300

    front added

---
+++ front/src/App.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="app-block">
+
+    <header>
+      <span>Алексеев Даниил</span> P3233<br>
+      вар. 33407
+    </header>
+
+    <main>
+      <div class="content-wrapper">
+        <div class="form-block">
+          <point-form @pushPoint="sendData" @clearPoints="clearData"></point-form>
+        </div>
+
+        <div class="response-block">
+          <point-table v-bind:points="points"></point-table>
+        </div>
+      </div>
+    </main>
+
+    <footer>
+
+    </footer>
+  </div>
+</template>
+
+<script>
+import PointTable from "@/Components/point-table";
+import PointForm from "@/Components/point-form";
+export default {
+  components: {PointForm, PointTable},
+  data(){
+    return{
+      points: [  ]
+    }
+  },
+  methods:{
+    sendData: function (newPoint){
+      this.points.push({x: newPoint.x, y: newPoint.y, r: newPoint.r, ishit: newPoint.ishit, time: newPoint.time});
+    },
+    clearData: function(){
+      this.points = []
+    }
+  }
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600&display=swap");
+* {
+  box-sizing: border-box;
+}
+
+html, body {
+  background: #343434;
+  margin: 0;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 300;
+  color: whitesmoke;
+  font-size: 20px;
+}
+
+header, main, footer{
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.app-block{
+  display: flex;
+  flex-direction: column;
+}
+
+header{
+  background: #8f8fff;
+  padding: 2px;
+  text-align: center;
+  font-size: 30px;
+}
+
+header > span {
+  font-weight: 500;
+}
+
+main{
+  padding: 10px;
+  height: 80vh;
+}
+
+.content-wrapper{
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+  grid-column-gap: 10px;
+}
+
+.form-block{
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 2;
+  padding: 5px;
+}
+
+.response-block{
+  grid-column-start: 2;
+  grid-column-end: 3;
+  grid-row-start: 1;
+  grid-row-end: 3;
+}
+</style>
---
+++ front/src/Components/point-form.vue
@@ -0,0 +1,122 @@
+<template>
+  <form v-on:submit.prevent>
+    <div class="x-radio">
+      <div class="wrappers">
+        <div class="value-label">X</div>
+        <radio-btn @radioChange="changeXRadioBtn" ref="xComponent"></radio-btn>
+      </div>
+    </div>
+
+    <div class="y-text">
+      <div class="wrappers">
+        <div class="value-label">Y</div>
+        <input type="text" placeholder="Type Y" v-model="newPoint.y">
+      </div>
+    </div>
+    <input type="text" placeholder="Type Y" v-model="newPoint.hit">
+    <div class="r-radio">
+      <div class="wrappers">
+        <div class="value-label">R</div>
+        <radio-btn @radioChange="changeRRadioBtn" ref="rComponent"></radio-btn>
+      </div>
+    </div>
+
+<!--    <input type="text" placeholder="Type Y" v-model="newPoint.hit">-->
+
+    <div class="btns-block">
+      <div class="main-btns-block">
+        <usual-button @click.native="sendData">send</usual-button>
+        <usual-button @click.native="resetForm">reset</usual-button>
+      </div>
+      <div class="side-btns-block">
+        <usual-button @click.native="clearData" btn-type="dangerous">clear</usual-button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+import UsualButton from "@/Components/UI/button";
+import RadioBtn from "@/Components/UI/radio-btn";
+
+export default {
+  name: "point-form",
+  components: {RadioBtn, UsualButton},
+  data(){
+    return{
+      newPoint: {
+        x: 0,
+        y: 0,
+        r: 0,
+        hit: 0
+      },
+      points: [  ]
+    }
+  },
+  methods:{
+    changeXRadioBtn: function (newX){
+      this.newPoint.x = newX;
+    },
+    changeRRadioBtn: function (newR){
+      this.newPoint.r = newR;
+    },
+    sendData: function (){
+      this.newPoint.time = Date.now();
+      this.newPoint.ishit = this.newPoint.hit === '1';
+      this.$emit('pushPoint', this.newPoint);
+    },
+    clearData: function (){
+      this.$emit('clearPoints')
+    },
+    resetForm: function (){
+      this.newPoint.x = 0;
+      this.newPoint.y = 0;
+      this.newPoint.r = 0;
+      this.newPoint.hit = 0;
+      this.$refs.xComponent.resetChoice();
+      this.$refs.rComponent.resetChoice();
+    }
+  }
+}
+</script>
+
+<style scoped>
+.x-radio, .y-text, .r-radio, .btns-block{
+  margin-top: 5px;
+  display: block;
+  width: 100%;
+  background: #202020;
+  padding: 10px;
+}
+
+.wrappers{
+  display: flex;
+}
+
+.value-label{
+  margin-left: 2px;
+  font-weight: 500;
+}
+
+.y-text > .wrappers > input{
+  margin-left: 10px;
+}
+
+.btns-block{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.side-btns-block {
+  text-align: right;
+}
+
+.main-btns-block {
+  text-align: left;
+}
+
+.main-btns-block > button:first-of-type, .side-btns-block > button:first-of-type {
+  margin: 0;
+}
+
+</style>
