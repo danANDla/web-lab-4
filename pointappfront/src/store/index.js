a671db8ac39552093ba7ad0fc230440630a48e0e
archdla <d.a.n.202021@gmail.com>
2022-01-26 14:32:53 +0300

    vuex state stored in a cookie

--- pointappfront/src/store/index.js
+++ pointappfront/src/store/index.js
@@ -1,8 +1,17 @@
 import { createStore } from 'vuex'
+import createPersistedState from "vuex-persistedstate";
+import * as Cookies from 'js-cookie'
 import {tokenModule} from "@/store/tokenModule";
 
 export default createStore({
   modules: {
     auth: tokenModule
-  }
+  },
+  plugins: [
+    createPersistedState({
+      // getState: (key) => Cookies.getJSON(key),
+      // setState: (key, state) => Cookies.set(key, state, { expires: 3, secure: true })
+      storage: window.sessionStorage
+    })
+  ]
 })
