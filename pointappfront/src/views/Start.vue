dd69a50c1ec2382725ff40104733c75fd6f49dda
archdla <d.a.n.202021@gmail.com>
2022-01-29 10:55:01 +0300

    added clock on start page

--- pointappfront/src/views/Start.vue
+++ pointappfront/src/views/Start.vue
@@ -6,6 +6,10 @@
     </header>
     <main>
       <div class="content-wrapper">
+        <div class="time-block">
+          <div id="clock"></div>
+          <div id="date"></div>
+        </div>
         <login @logIn="signIn" v-model:error="error"></login>
       </div>
     </main>
@@ -66,15 +70,22 @@ export default {
             else{
               this.error = "some_error";
             }
-            // if(data.toString() === "false") {
-            //   console.log("Bad response");
-            // }
-            // else{
-            //   console.log("Good response");
-            //   this.$router.push({name: "PointApp", params: {login: Login, pass: Pass, status: "ok"}});
-            // }
           });
+    },
+    update: function() {
+        let date = new Date(),
+            months = ["January","February","March","April","May","June","July","August","September","October","November","December"],
+            days = (date.getDate() < 10) ? '0' + date.getDate() : date.getDate(),
+            hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+            minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
+            seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+        document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
+        document.getElementById('date').innerHTML = days + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
     }
+  },
+  mounted() {
+    this.update();
+    setInterval(this.update, 11000);
   }
 }
 
@@ -89,5 +100,13 @@ main{
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+}
+
+.time-block{
+  width: 100%;
+  background: #202020;
+  padding: 23px;
+  display: block;
+  margin-bottom: 40px;
 }
 </style>
